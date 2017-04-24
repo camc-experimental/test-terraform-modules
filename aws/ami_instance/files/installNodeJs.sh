b275c56ac11ecc44e92b73a5831c465a5bae8c1e
Hongwei Tian <htian@us.ibm.com>
2017-04-24 09:30:17 -0500

    Add additional public key support

--- aws/ami_instance/files/installNodeJs.sh
+++ aws/ami_instance/files/installNodeJs.sh
@@ -20,7 +20,11 @@ set -o pipefail
 
 LOGFILE="/var/log/install_nodejs.log"
 
-DBADDRESS=${variable_1}
+TEMP_SSH_KEY=${variable_1}
+DBADDRESS=${variable_2}
+
+echo "---Add temp public key for ssh---" | tee -a $LOGFILE 2>&1
+echo $TEMP_SSH_KEY | tee -a .ssh/authorized_keys                                                                  >> $LOGFILE 2>&1 || { echo "---Failed to add temp public key for ssh---" | tee -a $LOGFILE; exit 1; }
 
 echo "---Install nodejs---" | tee -a $LOGFILE 2>&1
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -                                                    >> $LOGFILE 2>&1 || { echo "---Failed to run node script to set up repo---" | tee -a $LOGFILE; exit 1; }
